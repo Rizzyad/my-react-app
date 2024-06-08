1505223df3c038c15fd3e99fe6d85eac0a8897cd
rizzyad <rizzyadramadhan@gmail.com>
2024-06-08 14:40:12 +0700

    4-pengenalan-atomic-design

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import React from "react";
-
-const Button = ({ variant = "bg-slate-700", children }) => {
-  return (
-    <button
-      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
-      type="submit"
-    >
-      {children}
-    </button>
-  );
-};
+import Button from "./components/Elements/Button/Button";
+import Input from "./components/Elements/Input";
 
 const App = () => {
   return (
     <>
       <div className="flex justify-center bg-blue-600 min-h-screen items-center">
         <div className="flex gap-x-3">
-          <Button variant="bg-slate-700">Login</Button>
-          <Button variant="bg-red-700">Sign Up</Button>
-          <Button variant="bg-green-700">Hello World</Button>
+          <Button variant="bg-red-700">Login</Button>
+          <Button variant="bg-slate-700">Logout</Button>
         </div>
       </div>
     </>
---
+++ src/components/Elements/Button/Button.jsx
@@ -0,0 +1,12 @@
+const Button = ({ variant = "bg-slate-700", children }) => {
+  return (
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
