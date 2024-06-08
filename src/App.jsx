8b91705d8e89bec9591ad22f8be47e92fb493061
rizzyad <rizzyadramadhan@gmail.com>
2024-06-08 13:50:10 +0700

    2-all-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
+import React from "react";
+
 const App = () => {
   return (
     <>
       <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-        <button
-          className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-          type="submit"
-        >
-          Buy now
-        </button>
-        <button
-          className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-          type="submit"
-        >
-          Buy now
-        </button>
-        <button
-          className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-          type="submit"
-        >
-          Buy now
-        </button>
+        <div className="flex gap-x-3">
+          <Button></Button>
+          <Button></Button>
+          <Button></Button>
+          <Button></Button>
+          <ButtonBlack></ButtonBlack>
+          <ButtonRed></ButtonRed>
+        </div>
       </div>
     </>
   );
 };
 
 export default App;
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button
+        className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white"
+        type="submit"
+      >
+        Buy now
+      </button>
+    );
+  }
+}
+
+function ButtonBlack() {
+  return (
+    <button className="h-10 px-6 font-semibold rounded-md bg-black text-white">
+      Buy now
+    </button>
+  );
+}
+
+const ButtonRed = () => {
+  return (
+    <button
+      className="h-10 px-6 font-semibold rounded-md bg-red-700 text-white"
+      type="submit"
+    >
+      Buy now
+    </button>
+  );
+};
